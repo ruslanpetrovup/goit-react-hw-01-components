a95aa8f1de19eccc3f7d27e0c08f71884b587968
Ruslan <ruslanpetrov163@gmail.com>
2021-12-27 20:27:56 +0200

    correct friends

--- src/FriendsList/FriendList.js
+++ src/FriendsList/FriendList.js
@@ -6,7 +6,7 @@ const FriendList = ({ friends }) => {
     <ul class={style.friendList}>
           {friends.map(num => (
               <li class={style.item}>
-                  <span class={ num.isOnline  && style.status || style.offline}></span>
+                  <span class={ num.isOnline ? style.status : style.offline}></span>
           <img class={style.avatar} src={num.avatar} alt={num.name} width="48" />
           <p class="name">{num.name}</p>
         </li>
